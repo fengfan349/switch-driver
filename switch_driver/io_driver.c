bb13ea386b161f663439e1e60ff2c674b14f22bd
fengfan349 <hao349flash@gmail.com>
2017-05-25 02:41:33 -0700

    update

--- switch_driver/io_driver.c
+++ switch_driver/io_driver.c
@@ -1,3 +1,14 @@
 #include "io_driver.h"
 
+#define IMMR_BASE 0xff000000 //内部存储器物理基地址
+#define IMMR_LEN 0x100000  //内部存储器物理地址空间大小1MB
+
+#define IO_DRIVER_VERSION "0.0.1" //版本号
+#define IO_DRIVER_NAME    "io_driver" //设备名
+#define IO_MAJOR          250 //主设备号
+#define IO_MINOR          0 //次设备号
+
+/*自定义设备结构体*/
+
+
 
--- switch_driver/io_driver.h
+++ switch_driver/io_driver.h
@@ -27,5 +27,46 @@
 #include <asm/semaphore.h>
 
 
+/*寄存器数据类型 volatile*/
+#ifndef VUINT16
+typedef volatile unsigned short int VUINT16;
+#endif
+
+#ifndef VUINT32
+typedef volatile unsigned int 		VUINT32;
+#endif
+
+#define CAST(x) (x) //强制类型转换
+
+/* PIP register set */
+
+#define	PBDIR(base)	(CAST(VUINT32 *)((base) + 0x0AB8)) /* PB data dir */
+#define	PBPAR(base)	(CAST(VUINT32 *)((base) + 0x0ABC)) /* PB pin ass*/
+#define	PBODR(base)	(CAST(VUINT16 *)((base) + 0x0AC2)) /* PB open drain*/
+#define	PBDAT(base)	(CAST(VUINT32 *)((base) + 0x0AC4)) /* PB Data Reg*/
+
+/* Input/Output Port register */
+
+#define	PADIR(base)	(CAST(VUINT16 *)((base) + 0x0950)) /* PA data dir*/
+#define	PAPAR(base)	(CAST(VUINT16 *)((base) + 0x0952)) /* PA pin assign*/
+#define	PAODR(base)	(CAST(VUINT16 *)((base) + 0x0954)) /* PA open drain*/
+#define	PADAT(base)	(CAST(VUINT16 *)((base) + 0x0956)) /* PA data reg */
+
+#define	PCDIR(base)	(CAST(VUINT16 *)((base) + 0x0960)) /* PC data dir */
+#define	PCPAR(base)	(CAST(VUINT16 *)((base) + 0x0962)) /* PC pin assign*/
+#define	PCSO(base)	(CAST(VUINT16 *)((base) + 0x0964)) /* PC options */
+#define	PCDAT(base)	(CAST(VUINT16 *)((base) + 0x0966)) /* PC data reg */
+#define	PCINT(base)	(CAST(VUINT16 *)((base) + 0x0968)) /* PC intr ctrl */
+
+#define	PDDIR(base)	(CAST(VUINT16 *)((base) + 0x0970)) /* PD data dir */
+#define	PDPAR(base)	(CAST(VUINT16 *)((base) + 0x0972)) /* PD pin assign*/
+#define	PDDAT(base)	(CAST(VUINT16 *)((base) + 0x0976)) /* PD data reg */
+
+#define ECNCTL(base) 	(CAST(VUINT32 *)((base) + 0x0E40))
+#define IEVENT(base) 	(CAST(VUINT32 *)((base) + 0x0E44))
+#define MII_DATA(base)	(CAST(VUINT32 *)((base) + 0x0E80))
+#define MII_SPEED(base)	(CAST(VUINT32 *)((base) + 0x0E84))
+
+
 #endif
 
