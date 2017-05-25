5881c6b3569283c1af52f485def2150ff540541e
fengfan349 <hao349flash@gmail.com>
2017-05-25 02:17:02 -0700

    update0525

---
+++ switch_driver/io_driver.h
@@ -0,0 +1,31 @@
+#ifndef IO_DRIVER_H
+#define IO_DRIVER_H
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>		
+#include <linux/slab.h>		
+#include <linux/fs.h>			
+#include <linux/errno.h>		
+#include <linux/types.h>		
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h>		
+#include <linux/aio.h>
+#include <linux/ioport.h>
+#include <linux/cdev.h>
+#include <linux/ioctl.h> 		
+#include <linux/mm.h>
+#include <linux/delay.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+#include <asm/uaccess.h>	
+#include <asm/io.h>
+#include <asm/system.h>		
+#include <asm/page.h>
+#include <asm/semaphore.h>
+
+
+#endif
+
